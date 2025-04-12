318296b67ea3baab2543d654c96034a3c225ebb8
Andrey Barannikov <barannikov.av@phystech.edu>
2025-04-12 17:19:08 +0300

    Initial implementation of layers pages.

--- layers/views.py
+++ layers/views.py
@@ -5,7 +5,7 @@ def index(request):
     return render(request, "layers/index.html")
 
 def detail(request, layer_id):
-    return HttpResponse("You're looking at layer %s." % layer_id)
+    return render(request, f"layers/layer_{layer_id}.html")
 
 def test(request, layer_id):
     return HttpResponse("You're taking a test on the layer %s." % layer_id)
